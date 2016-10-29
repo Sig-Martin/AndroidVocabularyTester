01d683406a7d7f8cbef71b647306f171f84e8b1d
Shiyao <liushiyao2014@gmail.com>
2016-10-29 18:45:46 -0500

    Implement part of MainActivity

--- app/src/main/java/shiyao/com/vocabularyscorer/MainActivity.java
+++ app/src/main/java/shiyao/com/vocabularyscorer/MainActivity.java
@@ -1,15 +1,39 @@
 package shiyao.com.vocabularyscorer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Status currentStatus;
 
+    private Button begin;
+
+    private EditText totalNumberInput;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.begin = (Button) this.findViewById(R.id.begin);
+        this.totalNumberInput = (EditText) this.findViewById(R.id.total_count);
+        begin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String totalCount = MainActivity.this.totalNumberInput.getText().toString();
+
+                if (totalCount == null || totalCount.isEmpty() || Integer.valueOf(totalCount) <= 0) {
+                    Toast.makeText(MainActivity.this, "请输入一个正确数字", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                MainActivity.this.currentStatus = new Status(Integer.valueOf(totalCount));
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/shiyao/com/vocabularyscorer/ScorerActivity.java
@@ -0,0 +1,14 @@
+package shiyao.com.vocabularyscorer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ScorerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scorer);
+    }
+
+}
